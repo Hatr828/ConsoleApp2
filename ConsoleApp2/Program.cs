1f0785c1f3e0cd1c02e62d8372be404f67b45566
Hatr828 <hatr626@gmail.com>
2024-02-24 16:29:09 -0600

    5

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,99 +1,90 @@
 ﻿using System;
-using System.Linq;
 
-public class Item
+public class _string
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
-    public int Value { get; set; }
-    public int Weight { get; set; }
+    protected char[] line;
+    protected int lenght;
 
-    public Item(string name, int level, int value, int weight)
+    public _string(char[] line)
     {
-        Name = name;
-        Level = level;
-        Value = value;
-        Weight = weight;
-    }
-}
+        this.line = new char[line.Length];
+        lenght = line.Length;
 
-public class Player
-{
-    public string Name { get; set; }
-    private Item[] inventory;
-    private int count;
-
-    public Player(string name, int capacity)
-    {
-        Name = name;
-        inventory = new Item[capacity];
-        count = 0;
-    }
-
-    public void AddItem(Item item)
-    {
-        if (count < inventory.Length)
+        for (int i = 0; i < line.Length; i++)
         {
-            inventory[count++] = item;
-        }
-        else
-        {
-            Console.WriteLine("Inventory is full. Cannot add more items.");
+            this.line[i] = line[i];
         }
     }
-
-    public void RemoveItem(Item item)
+    public _string(string line)
     {
-        int index = Array.IndexOf(inventory, item);
-        if (index != -1)
+        this.line = new char[line.Length];
+        lenght = line.Length;
+
+        for (int i = 0; i < line.Length; i++)
         {
-            for (int i = index; i < count - 1; i++)
+            this.line[i] = line[i];
+        }
+    }
+    public _string(_string str)
+    {
+        this.line = new char[str.Length];
+        lenght = str.Length;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            this.line[i] = str[i];
+        }
+    }
+    public _string(_string str, uint n)
+    {
+        this.line = new char[str.Length];
+        lenght = str.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            this.line[i] = str[i];
+        }
+    }
+    public _string(char[] line, uint n)
+    {
+        this.line = new char[n];
+        lenght = Convert.ToUInt16(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            this.line[i] = line[i];
+        }
+    }
+    public _string(uint size)
+    {
+        line = new char[size];
+        lenght = Convert.ToUInt16(size);
+    }
+    public _string()
+    {
+        line = new char[25];
+    }
+
+    public char this[int number]
+    {
+        get
+        {
+                return line[number];
+        }
+        set
+        {
+            if (line.Length < number)
             {
-                inventory[i] = inventory[i + 1];
-            }
-            inventory[count - 1] = null;
-            count--;
-        }
-        else
-        {
-            Console.WriteLine("Item not found in the inventory.");
-        }
-    }
-
-    public int TotalWeight()
-    {
-        int totalWeight = 0;
-        foreach (var item in inventory)
-        {
-            if (item != null)
-            {
-                totalWeight += item.Weight;
-            }
-        }
-        return totalWeight;
-    }
-
-    public int TotalValue()
-    {
-        int totalValue = 0;
-        foreach (var item in inventory)
-        {
-            if (item != null)
-            {
-                totalValue += item.Value;
+                line[number] = value;
             }
         }
-        return totalValue;
     }
-
-    public Item[] FindItemsByLevel(int level)
+    public int Length
     {
-        return inventory.Where(item => (item != null && item.Level == level)).ToArray();       // скопировал с интарнета
-    }
-
-    public Item[] FindItemsByValue(int value)
-    {
-        return inventory.Where(item => item != null && item.Value == value).ToArray(); // скопировал с интарнета
+        get
+        {
+            return lenght;
+        }
     }
 }
 
@@ -101,25 +92,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Player player = new Player("Player1", 10);
-
-        Item sword = new Item("Sword", 10, 100, 5);
-        Item armor = new Item("Armar", 15, 200, 10);
-        Item potion = new Item("Potion", 5, 50, 1);
-
-        player.AddItem(sword);
-        player.AddItem(armor);
-        player.AddItem(potion);
-
-        Console.WriteLine($"Total weight of player inventary: {player.TotalWeight()}");
-        Console.WriteLine($"Total value of player inventary: {player.TotalValue()}");
-
-        Console.WriteLine("Items with level 10: ");
-        Item[] level10Items = player.FindItemsByLevel(10);
-        foreach (var item in level10Items)
-        {
-            Console.WriteLine($"{item.Name} - Level: {item.Level}, Value: {item.Value}, Weight: {item.Weight}");
-        }
+        _string str = new _string("Hello world");
+        Console.WriteLine($"String: {str[2]}. Length: {str.Length}");
+        _string str2 = new _string(str, 6);
+        Console.WriteLine($"String: {str2[4]}. Length: {str2.Length}");
+        _string str3 = new _string(10);
+        Console.WriteLine($"String: {str3[5]}. Length: {str3.Length}");
 
         Console.ReadLine();
     }
