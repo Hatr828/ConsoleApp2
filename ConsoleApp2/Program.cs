602e768bb0d7e12a24c20083af5184ee1a891720
Hatr828 <hatr626@gmail.com>
2024-03-05 20:23:39 -0600

    8

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,103 +1,88 @@
 ﻿using System;
 
-public class _string
+class Vehicle
 {
-    protected char[] line;
-    protected int lenght;
+    public string Type { get; set; }
 
-    public _string(char[] line)
+    public virtual string Description
     {
-        this.line = new char[line.Length];
-        lenght = line.Length;
-
-        for (int i = 0; i < line.Length; i++)
-        {
-            this.line[i] = line[i];
-        }
-    }
-    public _string(string line)
-    {
-        this.line = new char[line.Length];
-        lenght = line.Length;
-
-        for (int i = 0; i < line.Length; i++)
-        {
-            this.line[i] = line[i];
-        }
-    }
-    public _string(_string str)
-    {
-        this.line = new char[str.Length];
-        lenght = str.Length;
-
-        for (int i = 0; i < str.Length; i++)
-        {
-            this.line[i] = str[i];
-        }
-    }
-    public _string(_string str, uint n)
-    {
-        this.line = new char[str.Length];
-        lenght = str.Length;
-
-        for (int i = 0; i < n; i++)
-        {
-            this.line[i] = str[i];
-        }
-    }
-    public _string(char[] line, uint n)
-    {
-        this.line = new char[n];
-        lenght = Convert.ToUInt16(n);
-
-        for (int i = 0; i < n; i++)
-        {
-            this.line[i] = line[i];
-        }
-    }
-    public _string(uint size)
-    {
-        line = new char[size];
-        lenght = Convert.ToUInt16(size);
-    }
-    public _string()
-    {
-        line = new char[25];
+        get { return "This is a vehicle."; }
     }
 
-    public char this[int number]
+    public virtual void Move()
     {
-        get
-        {
-                return line[number];
-        }
-        set
-        {
-            if (line.Length < number)
-            {
-                line[number] = value;
-            }
-        }
+        Console.WriteLine("The vehicle is moving.");
     }
-    public int Length
+
+    public Vehicle(string type)
     {
-        get
-        {
-            return lenght;
-        }
+        Type = type;
     }
+}
+
+class Truck : Vehicle
+{
+    public Truck(string type) : base(type) { }
+
+    public override string Description
+    {
+        get { return "This is a truck."; }
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("The truck is transporting goods.");
+    }
+}
+
+class Aircraft : Vehicle
+{
+    public Aircraft(string type) : base(type) { }
+
+    public override string Description
+    {
+        get { return "This is an aircraft."; }
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("The aircraft is flying.");
+    }
+}
+
+class FighterJet : Aircraft
+{
+    public FighterJet(string type) : base(type) { }
+
+    public override void Move()
+    {
+        Console.WriteLine("The fighter jet is engaging in combat maneuvers.");
+    }
+
+    public void PerformAirStrike()
+    {
+        base.Move(); 
+        Console.WriteLine("Launching air strike!");
+    }
+}
+
+class Helicopter : Aircraft
+{
+    public Helicopter(string type) : base(type) { }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        _string str = new _string("Hello world");
-        Console.WriteLine($"String: {str[2]}. Length: {str.Length}");
-        _string str2 = new _string(str, 6);
-        Console.WriteLine($"String: {str2[4]}. Length: {str2.Length}");
-        _string str3 = new _string(10);
-        Console.WriteLine($"String: {str3[5]}. Length: {str3.Length}");
+        Aircraft aircraft = new Aircraft("rrrr");
+        Console.WriteLine(aircraft.Description);
+        aircraft.Move();
+        Console.WriteLine();
+
+        Helicopter helicopter = new Helicopter("hhh");
+        Console.WriteLine(helicopter.Description);
+        helicopter.Move();
 
         Console.ReadLine();
     }
