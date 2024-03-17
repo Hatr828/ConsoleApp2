d3754102963c6135d864df4423e068f885c73bb8
Hatr828 <hatr626@gmail.com>
2024-03-17 09:15:18 -0500

    11

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,65 +1,59 @@
 ﻿using System;
 
-public class Processor
-{
-    public string Model { get; }
-
-    public Processor(string model)
-    {
-        Model = model;
-    }
-}
-
-public class Motherboard
-{
-    private Processor _processor;
-    private int _ramSizeGB;
-
-    public Processor Processor
-    {
-        get { return _processor; }
-        set { _processor = value; }
-    }
-
-    public int RamSizeGB
-    {
-        get { return _ramSizeGB; }
-        set
-        {
-            if (value <= 0)
-            {
-                throw new ArgumentException("argument < 0");
-            }
-            _ramSizeGB = value;
-        }
-    }
-
-    public Motherboard(Processor processor, int ramSizeGB)
-    {
-        Processor = processor;
-        RamSizeGB = ramSizeGB;
-    }
-
-    public string GetName()
-    {
-        return $"{Processor.Model},{RamSizeGB}";
-    }
-}
-
 class Program
 {
-    static void Main(string[] args)
+    delegate bool SortDelegate(int a);
+
+    static bool isEven(int a)
     {
-        Processor processor = new Processor("Procea");
-        try
-        {
-            Motherboard motherboard = new Motherboard(processor, 16);
-            Console.WriteLine(motherboard.GetName());
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
+        return a % 2 == 0;
     }
 
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int[] filtered1 = FilterArray(numbers, (x) => x % 2 == 0);
+        PrintArray(filtered1);
+
+        int[] filtered2 = FilterArray(numbers, delegate (int x) { return x % 2 != 0; });
+        PrintArray(filtered2);
+
+        int[] filtered3 = FilterArray(numbers, isEven);
+        PrintArray(filtered3);
+
+        Console.ReadLine();
+    }
+    static int[] FilterArray(int[] array, SortDelegate sortFunc)
+    {
+        int count = 0;
+        foreach (var item in array)
+        {
+            if (sortFunc(item))
+            {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+        foreach (var item in array)
+        {
+            if (sortFunc(item))
+            {
+                result[index++] = item;
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
 }
