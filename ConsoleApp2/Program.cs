5a308a43f6f28e7e0a9012a0635b9ade2c5a6bc4
Hatr828 <hatr626@gmail.com>
2024-03-20 19:11:22 -0500

    12

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,59 +1,54 @@
 ﻿using System;
 
+public interface ICipher
+{
+    string Encode(string input);
+    string Decode(string input);
+}
+
+public class Cipher : ICipher
+{
+    private const int shift = 2; // Количество бит для сдвига
+
+    public string Encode(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            charArray[i] = (char)(charArray[i] << shift);
+        }
+
+        return new string(charArray);
+    }
+
+    public string Decode(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            charArray[i] = (char)(charArray[i] >> shift);
+        }
+
+        return new string(charArray);
+    }
+}
+
 class Program
 {
-    delegate bool SortDelegate(int a);
-
-    static bool isEven(int a)
-    {
-        return a % 2 == 0;
-    }
-
     static void Main(string[] args)
     {
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        ICipher cipher = new Cipher();
 
-        int[] filtered1 = FilterArray(numbers, (x) => x % 2 == 0);
-        PrintArray(filtered1);
+        string originalString = "Hello, World!";
+        string encryptedString = cipher.Encode(originalString);
+        string decryptedString = cipher.Decode(encryptedString);
 
-        int[] filtered2 = FilterArray(numbers, delegate (int x) { return x % 2 != 0; });
-        PrintArray(filtered2);
-
-        int[] filtered3 = FilterArray(numbers, isEven);
-        PrintArray(filtered3);
+        Console.WriteLine("1    " + originalString);
+        Console.WriteLine("2    " + encryptedString);
+        Console.WriteLine("3    " + decryptedString);
 
         Console.ReadLine();
-    }
-    static int[] FilterArray(int[] array, SortDelegate sortFunc)
-    {
-        int count = 0;
-        foreach (var item in array)
-        {
-            if (sortFunc(item))
-            {
-                count++;
-            }
-        }
-
-        int[] result = new int[count];
-        int index = 0;
-        foreach (var item in array)
-        {
-            if (sortFunc(item))
-            {
-                result[index++] = item;
-            }
-        }
-
-        return result;
-    }
-
-    static void PrintArray(int[] array)
-    {
-        foreach (var item in array)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine();
     }
 }
